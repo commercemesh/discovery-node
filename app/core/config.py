0c26a60485739c7d84ff1ec2286b0c07657fc282
Shiv Agarwal <shiv@Shivs-Mac-Studio.local>
2025-07-21 17:17:55 -0700

    preparing for opensource

--- app/core/config.py
+++ app/core/config.py
@@ -23,14 +23,14 @@ class Settings(BaseSettings):
     EMBEDDING_UPDATE_INTERVAL: int = int(os.getenv("EMBEDDING_UPDATE_INTERVAL", "3600"))
     CLEANUP_INTERVAL: int = int(os.getenv("CLEANUP_INTERVAL", "3600"))
     DATA_DIR: str = os.getenv(
-        "DATA_DIR", "/Users/shiv/Documents/CMP/new/discovery-node/"
+        "DATA_DIR", "/tmp/discovery-node/"
     )
     TRIGGER_INGESTION_ON_STARTUP: bool = (
         os.getenv("TRIGGER_INGESTION_ON_STARTUP", "false").lower() == "true"
     )
     INGESTION_CONFIG_PATH: str = os.getenv(
         "INGESTION_CONFIG_PATH",
-        "/Users/shiv/Documents/CMP/new/discovery-node/ingestion.yaml",
+        "/tmp/ingestion.yaml",
     )
     # Other settings
     DEBUG: bool = os.getenv("DEBUG", "true").lower() == "true"
